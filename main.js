59dd2d7f34e9d4b4caea022badf7a3ca450bbe69
RLAVAN-RAGHURAM <70435014+RLAVAN-RAGHURAM@users.noreply.github.com>
2020-09-19 16:42:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,52 @@
-function bedroom(){
-window.location="./bedroom/bedroom.html";
+video="";
+objects=[];
+status="";
+percent="";
+function preload(){
+video=createVideo('video.mp4');
+video.hide();
 }
-function bathroom(){
-window.location="./bathroom/bathroom.html";
+function setup(){
+canvas=createCanvas(480,380);
+canvas.center();
 }
-function tv(){
-window.location="./TV/TV.html";
+function start(){
+objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML="Status : Detecting Object";
 }
-function FishTank(){
-window.location="./Fishtank/FishTank.html";
+function modelLoaded(){
+console.log("modeloaded");
+status=true;
+video.volume(0);
+video.speed(1);
+video.loop();
 }
-function chair(){
-window.location="./Chair/chair.html";
+function gotResult(error,results){
+if (error){
+console.log(error);
 }
-function tab(){
-window.location="./tab/tab.html";
+console.log(results);
+objects=results;
 }
-function person(){
-window.location="./person/person.html";
+    
+
+
+function draw(){
+image(video,0,0,480,380); 
+if(status!="")
+{
+objectDetector.detect(video,gotResult);
+for(i=0;i<objects.length;i++){
+document.getElementById("Number_of_object").innerHTML="Number of objects in the Screen : "+objects.length;
+document.getElementById("status").innerHTML="Object Detected";
+fill("#1cb8b8");
+percent=floor(objects[i].confidence*100);
+text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+noFill();
+stroke("#1cb8b8");
+rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
 }
-function boat(){
-window.location="./Boat/boat.html";
 }
-function apple(){
-window.location="./Apple_Basket/apple.html";
 }
-function book(){
-window.location="./Book/book.html"
-}
+
+
